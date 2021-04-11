4fa3a193c9a2fc7701dfd1fe691e2902a8ad38ab
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-11 16:45:11 +0300

    Get flickrHomePageRequest

--- app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.github.kirillmokretsov.photogallery.api.FlickrApi
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -25,6 +26,7 @@ class PhotoGalleryFragment : Fragment() {
                 .build()
 
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
     }
 
     override fun onCreateView(
