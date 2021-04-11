1c7ffdb9848af2c2dc8a9d0858d40fb025a52321
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-11 17:11:51 +0300

    Create fun fetchContents

--- app/src/main/java/io/github/kirillmokretsov/photogallery/FlickrFetchr.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/FlickrFetchr.kt
@@ -1,6 +1,12 @@
 package io.github.kirillmokretsov.photogallery
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.github.kirillmokretsov.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -17,6 +23,25 @@ class FlickrFetchr {
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String> {
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received: ${response.body()}")
+                responseLiveData.value = response.body()
+            }
+        })
+
+        return responseLiveData
+
     }
 
 }
