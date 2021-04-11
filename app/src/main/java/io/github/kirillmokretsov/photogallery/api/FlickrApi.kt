222aae12ae68c8903be3801944ed5295689ab553
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-11 16:30:52 +0300

    Create FlickrApi.kt

---
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/api/FlickrApi.kt
@@ -0,0 +1,11 @@
+package io.github.kirillmokretsov.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET("/")
+    fun fetchContents(): Call<String>
+
+}
