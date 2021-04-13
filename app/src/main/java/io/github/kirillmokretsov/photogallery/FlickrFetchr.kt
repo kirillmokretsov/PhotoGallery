f72f8595e32bd42c68689787107e4039207f72ef
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-13 12:33:49 +0300

    Add fetchPhotos for LoadCallback

--- app/src/main/java/io/github/kirillmokretsov/photogallery/FlickrFetchr.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/FlickrFetchr.kt
@@ -37,7 +37,33 @@ class FlickrFetchr(private val flickrApi: FlickrApi) {
                 callback.onResult(galleryItems, if (page == 1) null else page, page + 1)
             }
         })
+    }
 
+    fun fetchPhotos(
+        page: Int,
+        callback: PageKeyedDataSource.LoadCallback<Int, GalleryItem>
+    ) {
+        val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos(page)
+
+        flickrRequest.enqueue(object : Callback<FlickrResponse> {
+            override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(
+                call: Call<FlickrResponse>,
+                response: Response<FlickrResponse>
+            ) {
+                Log.d(TAG, "Response received: ${response.body()}")
+                val flickrResponse: FlickrResponse? = response.body()
+                val photoResponse: PhotoResponse? = flickrResponse?.photos
+                var galleryItems: List<GalleryItem> = photoResponse?.galleryItems ?: mutableListOf()
+                galleryItems = galleryItems.filterNot {
+                    it.url.isBlank()
+                }
+                callback.onResult(galleryItems, page + 1)
+            }
+        })
     }
 
 }
