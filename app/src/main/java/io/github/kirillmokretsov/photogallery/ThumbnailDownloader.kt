5deb6bf7178ff35d31e531bb49b97b3399ee33c5
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-13 15:42:29 +0300

    Create constants and params

--- app/src/main/java/io/github/kirillmokretsov/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/ThumbnailDownloader.kt
@@ -1,16 +1,23 @@
 package io.github.kirillmokretsov.photogallery
 
+import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import io.github.kirillmokretsov.photogallery.api.FlickrApi
+import java.util.concurrent.ConcurrentHashMap
 
 private const val TAG = "ThumbnailDownloader"
+private const val MESSAGE_DOWNLOAD = 0
 
 class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
 
     private var hasQuit = false
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T, String>()
+    private val flickrFetchr = FlickrFetchr(FlickrApi.newInstance())
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun setup() {
