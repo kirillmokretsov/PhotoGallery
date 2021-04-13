68a1b47b7f91c1e529c8a3d1793b4503f529bf5c
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-13 12:10:39 +0300

    Update FlickrApi.kt to use pages

--- app/src/main/java/io/github/kirillmokretsov/photogallery/api/FlickrApi.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/api/FlickrApi.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FlickrApi {
 
@@ -12,17 +13,19 @@ interface FlickrApi {
                 "&api_key=f056d948cffa64689b6eb4c46d46d5eb" +
                 "&format=json" +
                 "&nojsoncallback=1" +
-                "&extras=url_s"
+                "&extras=url_s" +
+                "&per_page=15" +
+                "page={id}"
     )
-    fun fetchPhotos(): Call<FlickrResponse>
+    fun fetchPhotos(@Path("id") page: Int): Call<FlickrResponse>
 
     companion object {
         fun newInstance(): FlickrApi =
             Retrofit.Builder()
-            .baseUrl("https://api.flickr.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(FlickrApi::class.java)
+                .baseUrl("https://api.flickr.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(FlickrApi::class.java)
     }
 
 }
