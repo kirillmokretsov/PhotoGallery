03e042d7d15ae93f40061bdf5d324155efcaa2e4
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-13 13:25:13 +0300

    Create itemCallback to compare GalleryItems

--- app/src/main/java/io/github/kirillmokretsov/photogallery/GalleryItem.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/GalleryItem.kt
@@ -1,9 +1,21 @@
 package io.github.kirillmokretsov.photogallery
 
+import androidx.recyclerview.widget.DiffUtil
 import com.google.gson.annotations.SerializedName
 
 data class GalleryItem(
     var title: String = "",
     var id: String = "",
     @SerializedName("url_s") var url: String = ""
-)
+) {
+    val itemCallback: DiffUtil.ItemCallback<GalleryItem> = CustomItemCallback()
+
+    class CustomItemCallback : DiffUtil.ItemCallback<GalleryItem>() {
+        override fun areItemsTheSame(oldItem: GalleryItem, newItem: GalleryItem): Boolean =
+            oldItem.url == newItem.url
+
+
+        override fun areContentsTheSame(oldItem: GalleryItem, newItem: GalleryItem): Boolean =
+            oldItem.title == newItem.title && oldItem.id == newItem.id
+    }
+}
