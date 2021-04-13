8662e94b599b519e80e7573c93bc98349124b399
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-13 14:59:43 +0300

    Create ThumbnailDownloader.kt

---
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/ThumbnailDownloader.kt
@@ -0,0 +1,21 @@
+package io.github.kirillmokretsov.photogallery
+
+import android.os.HandlerThread
+import android.util.Log
+
+private const val TAG = "ThumbnailDownloader"
+
+class ThumbnailDownloader<in T> : HandlerThread(TAG) {
+
+    private var hasQuit = false
+
+    override fun quit(): Boolean {
+        hasQuit = true
+        return super.quit()
+    }
+
+    fun queueThumbnail(target: T, url: String) {
+        Log.i(TAG, "Got a URL: $url")
+    }
+
+}
