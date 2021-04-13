72d11e54ebf9f8fc288dc85d03457243caa87f6f
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-13 12:35:14 +0300

    Create GalleryItemDataSource.kt

--- app/src/main/java/io/github/kirillmokretsov/photogallery/GalleryItemDataSource.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/GalleryItemDataSource.kt
@@ -1,25 +1,32 @@
 package io.github.kirillmokretsov.photogallery
 
-import android.content.Context
 import androidx.paging.PageKeyedDataSource
 import io.github.kirillmokretsov.photogallery.api.FlickrApi
 
-class GalleryItemDataSource(flickrApi: FlickrApi, context: Context) :
+class GalleryItemDataSource(flickrApi: FlickrApi) :
     PageKeyedDataSource<Int, GalleryItem>() {
+
+    private val flickrFetchr = FlickrFetchr(flickrApi)
 
     override fun loadInitial(
         params: LoadInitialParams<Int>,
         callback: LoadInitialCallback<Int, GalleryItem>
     ) {
+        flickrFetchr.fetchPhotos(1, callback)
+    }
+
+    override fun loadBefore(
+        params: LoadParams<Int>,
+        callback: LoadCallback<Int, GalleryItem>
+    ) {
         TODO("Not yet implemented")
     }
 
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, GalleryItem>) {
-        TODO("Not yet implemented")
-    }
-
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, GalleryItem>) {
-        TODO("Not yet implemented")
+    override fun loadAfter(
+        params: LoadParams<Int>,
+        callback: LoadCallback<Int, GalleryItem>
+    ) {
+        flickrFetchr.fetchPhotos(params.key, callback)
     }
 
 }
