7645532abcad69a1f204b63ab5a924c67b73bd7a
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-16 10:01:42 +0300

    Create searchPagedList

--- app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryViewModel.kt
@@ -9,19 +9,28 @@ import java.util.concurrent.Executors
 
 class PhotoGalleryViewModel : ViewModel() {
 
-    private val galleryItemDataSourceFactory = GalleryItemDataSourceFactory(FlickrApi.newInstance())
+    private val flickrApi = FlickrApi.newInstance()
+
+    private val galleryItemDataSourceFactory = GalleryItemDataSourceFactory(flickrApi)
     private val config = PagedList.Config.Builder()
         .setEnablePlaceholders(true)
         .setInitialLoadSizeHint(30)
         .setPageSize(15)
         .setPrefetchDistance(12)
         .build()
-    private val executor = Executors.newFixedThreadPool(5)
-
+    private val galleryItemExecutor = Executors.newFixedThreadPool(5)
     val galleryItemPagedList: LiveData<PagedList<GalleryItem>> =
         LivePagedListBuilder(
             galleryItemDataSourceFactory,
             config
-        ).setFetchExecutor(executor).build()
+        ).setFetchExecutor(galleryItemExecutor).build()
+
+    private val searchDataSourceFactory = SearchDataSourceFactory(flickrApi, "galaxy")
+    private val searchExecutor = Executors.newFixedThreadPool(5)
+    val searchPagedList: LiveData<PagedList<GalleryItem>> =
+        LivePagedListBuilder(
+            searchDataSourceFactory,
+            config
+        ).setFetchExecutor(searchExecutor).build()
 
 }
