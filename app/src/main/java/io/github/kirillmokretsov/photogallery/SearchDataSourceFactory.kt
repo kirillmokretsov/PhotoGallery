75abd2c963c33b8234e81e19bacf702e826af0ed
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-16 09:57:57 +0300

    Create SearchDataSourceFactory.kt

---
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/SearchDataSourceFactory.kt
@@ -0,0 +1,9 @@
+package io.github.kirillmokretsov.photogallery
+
+import androidx.paging.DataSource
+import io.github.kirillmokretsov.photogallery.api.FlickrApi
+
+class SearchDataSourceFactory(private val flickrApi: FlickrApi, private val query: String) :
+    DataSource.Factory<Int, GalleryItem>() {
+    override fun create(): DataSource<Int, GalleryItem> = SearchDataSource(flickrApi, query)
+}
