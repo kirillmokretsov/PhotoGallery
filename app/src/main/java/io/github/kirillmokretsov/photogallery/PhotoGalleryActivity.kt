b597c39d9db36c172a90935fe7f0cd75e6bc8e29
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-17 09:26:44 +0300

    Create fun newIntent()

--- app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryActivity.kt
@@ -1,5 +1,7 @@
 package io.github.kirillmokretsov.photogallery
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,5 +15,9 @@ class PhotoGalleryActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance()).commit()
         }
+    }
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, PhotoGalleryActivity::class.java)
     }
 }
