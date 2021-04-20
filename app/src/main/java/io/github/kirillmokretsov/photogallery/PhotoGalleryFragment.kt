739997254b77a6d1d150a51e6dc58a972dd268fb
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-20 13:02:51 +0300

    Make work be periodic instead of one time

--- app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/PhotoGalleryFragment.kt
@@ -14,12 +14,14 @@ import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import com.squareup.picasso.Picasso
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "PhotoGalleryFragment"
+private const val POLL_WORK = "POLL_WORK"
 
 class PhotoGalleryFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener {
 
@@ -36,16 +38,6 @@ class PhotoGalleryFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener
         photoGalleryViewModel =
             ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
                 .get(PhotoGalleryViewModel::class.java)
-
-        // TODO: fix, not working
-        // Connected to Wi-Fi but android thinks it is metered
-        val constraints = Constraints.Builder()
-//            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .build()
-        val workRequest = OneTimeWorkRequest.Builder(PollWorker::class.java)
-            .setConstraints(constraints)
-            .build()
-        WorkManager.getInstance(requireContext()).enqueue(workRequest)
     }
 
     override fun onCreateView(
@@ -120,18 +112,39 @@ class PhotoGalleryFragment : Fragment(), ViewTreeObserver.OnGlobalLayoutListener
 
         val toggleItem = menu.findItem(R.id.menu_item_toggle_polling)
         val isPolling = QueryPreferences.isPoling(requireContext())
-        val toggleItemTitle = if(isPolling) R.string.stop_polling else R.string.start_polling
+        val toggleItemTitle = if (isPolling) R.string.stop_polling else R.string.start_polling
         toggleItem.setTitle(toggleItemTitle)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.menu_item_clear -> {
-                photoGalleryViewModel.fetchPhotos("")
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.menu_item_clear -> {
+            photoGalleryViewModel.fetchPhotos("")
+            true
         }
+        R.id.menu_item_toggle_polling -> {
+            val isPolling = QueryPreferences.isPoling(requireContext())
+            if (isPolling) {
+                WorkManager.getInstance(requireContext()).cancelUniqueWork(POLL_WORK)
+                QueryPreferences.setPoling(requireContext(), false)
+            } else {
+                val constraints = Constraints.Builder()
+                    // TODO: android thinks it is still metered
+//                        .setRequiredNetworkType(NetworkType.UNMETERED)
+                    .build()
+                val periodicRequest =
+                    PeriodicWorkRequest.Builder(PollWorker::class.java, 15, TimeUnit.MINUTES)
+                        .setConstraints(constraints).build()
+                WorkManager.getInstance(requireContext()).enqueueUniquePeriodicWork(
+                    POLL_WORK,
+                    ExistingPeriodicWorkPolicy.KEEP,
+                    periodicRequest
+                )
+                QueryPreferences.setPoling(requireContext(), true)
+            }
+            activity?.invalidateOptionsMenu()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     class PhotoHolder(private val itemImageView: ImageView) :
