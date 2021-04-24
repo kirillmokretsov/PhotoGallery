871424ee1ed3684313ad8a3f56fc24c39b57d7f0
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-24 11:22:28 +0300

    Create NotificationReceiver.kt

---
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/NotificationReceiver.kt
@@ -0,0 +1,16 @@
+package io.github.kirillmokretsov.photogallery
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "NotificationReceiver"
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent != null) {
+            Log.i(TAG, "received broadcast: ${intent.action}")
+        }
+    }
+}
