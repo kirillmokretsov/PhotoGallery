4e408011291f1f9a34635566b89903d907567090
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-24 13:02:33 +0300

    Add owner field and URL of photo's page

--- app/src/main/java/io/github/kirillmokretsov/photogallery/GalleryItem.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/GalleryItem.kt
@@ -1,13 +1,21 @@
 package io.github.kirillmokretsov.photogallery
 
+import android.net.Uri
 import androidx.recyclerview.widget.DiffUtil
 import com.google.gson.annotations.SerializedName
 
 data class GalleryItem(
     var title: String = "",
     var id: String = "",
-    @SerializedName("url_s") var url: String = ""
+    @SerializedName("url_s") var url: String = "",
+    @SerializedName("owner") var owner: String = ""
 ) {
+    val photoPageUri: Uri
+        get() = Uri.parse("https://www.flickr.com/photos")
+            .buildUpon()
+            .appendPath(owner)
+            .appendPath(id)
+            .build()
 
     companion object {
         val itemCallback: DiffUtil.ItemCallback<GalleryItem> = CustomItemCallback()
