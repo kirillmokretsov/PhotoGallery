3c658ea379543b8b703af0fb1048834b2e06990a
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-24 11:58:37 +0300

    Cancel notification if application is running

--- app/src/main/java/io/github/kirillmokretsov/photogallery/VisibleFragment.kt
+++ app/src/main/java/io/github/kirillmokretsov/photogallery/VisibleFragment.kt
@@ -1,21 +1,23 @@
 package io.github.kirillmokretsov.photogallery
 
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+
+private const val TAG = "VisibleFragment"
 
 abstract class VisibleFragment : Fragment() {
 
     private val onShowNotification = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            Toast.makeText(
-                requireContext(),
-                "Got a broadcast: ${intent?.action}",
-                Toast.LENGTH_LONG
-            ).show()
+            // If we receive this, we-re visible, so cancel the notification
+            Log.i(TAG, "cancelling notification")
+            resultCode = Activity.RESULT_CANCELED
         }
     }
 
